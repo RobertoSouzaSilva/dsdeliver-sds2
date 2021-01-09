6954027efac7007ff628f91b57d082b26b568099
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-09 13:58:57 -0300

    finalizado front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,3 +1,6 @@
+import { ToastContainer  } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import './App.css';
 import Navbar from './navbar';
@@ -6,9 +9,10 @@ import Routes from './routes';
 function App() {
   return (
     <div >
-
+        <>
         <Routes /> 
-      
+        <ToastContainer />
+        </>
     </div>
   );
 }

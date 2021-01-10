709a39f0132d118db96281ba0eff02a1b5312727
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-09 23:38:16 -0300

    correções para deploys do front

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,7 +3,6 @@ import { ToastContainer  } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import './App.css';
-import Navbar from './navbar';
 import Routes from './routes';
 
 function App() {

2d0540dc69bd50ab025abe6ab82aae75fb2c8bbd
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-11 21:02:39 -0300

    add listagem de pedidos

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 
 function Header() {
+
+  const navigation = useNavigation();
+
+    const HandleOnPress = () =>{
+      navigation.navigate('Home');
+    }
+
+
   return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} />
-        <Text style={styles.text}>Ds Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={HandleOnPress}>
+      <View style={styles.container}>
+          <Image source={require('../assets/logo.png')} />
+          <Text style={styles.text}>Ds Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
